e582aa232ba0556f9b1c9e9642e50249788eeb89
Vinicius <yinioliveira@gmail.com>
2022-09-07 13:08:38 -0300

    Criação de novas rotas e templates HTML
    Criação das rotas e arquivos HTML para as funcionalidades de cadastro e login

--- src/app.py
+++ src/app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+# Rota da página inicial
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Rota da página de cadastro
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+# Rota da página de login
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     debug = True
