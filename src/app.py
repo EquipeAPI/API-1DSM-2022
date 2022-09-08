5bd650ed4c67e47ca1c82bb1ea60bf8f13046d93
brunodenardo <brunoden.bd@gmail.com>
2022-09-08 19:19:16 -0300

    Adição das rotas '/deposito' e '/saque' e seus respectivos htmls provisórios.

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, redirect, request, session, url_for, flash
-from flask_mysqldb import MySQL
+#from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.secret_key = 'aonainfinnBFNFOANOnasfononfsa' #Chave de segurança da session
 
-
+'''
 # Configurações do banco de dados
 app.config['MYSQL_HOST'] = ''
 app.config['MYSQL_USER'] = ''
@@ -13,7 +13,7 @@ app.config['MYSQL_DB'] = ''
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
-
+'''
 
 # Rota da página de login (que é a página inicial)
 @app.route('/', methods=['POST', 'GET']) # As duas rotas acionaram a mesma função
@@ -23,7 +23,6 @@ def login():
         return redirect(url_for(home))'''
     if request.method == 'POST': #Se a pessoa apertar o botão 'ENTRAR' do forms
         nome = request.form['nome'] #Adicionando a uma variável python a informação do input nome do forms
-        senha = request.form['senha'] #Adicionando a uma variável python a informação do input senha do forms
         session['nome'] = nome #Criando uma session para transportar essa informação de maneira segura entre as rotas
         return redirect(url_for('home'))
         '''
@@ -71,6 +70,45 @@ def loggout():
         flash(f'{nome}, você saiu da sua conta com sucesso.', 'info') # Criando uma mensagem que vai ser mostrada na pagina login
     session.pop('nome', None) # Apagando as informações armazenadas na session['nome']
     return redirect(url_for('login')) # Redireciona para o login.
+
+
+@app.route('/deposito', method = ['POST', 'GET'])
+def deposito():
+    if 'name' in session:
+        if request.method == 'POST':
+            deposito = request.form['deposito']
+            return redirect(url_for('home'))
+
+
+            '''
+            ==========Possível inserção da função depósito usando arquivo .py do modelo==========
+            modelo.deposito(deposito)
+        
+            '''
+        else:
+            return render_template('deposito.html')
+    else:
+        return redirect(url_for('login'))
+
+
+@app.route('/saque', method = ['POST', 'GET'])
+def saque():
+    if 'name' in session:
+        if request.method == 'POST':
+            deposito = request.form['saque']
+            return redirect(url_for('home'))
+
+
+            '''
+            ==========Possível inserção da função saque usando arquivo .py do modelo==========
+            modelo.saque(saque)
+        
+            '''
+        else:
+            return render_template('saque.html')
+    else:
+        return redirect(url_for('login'))
+
 
 
 if __name__ == '__main__':
