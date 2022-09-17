0942a50f74ea5e81e1ae14f7ea5e19a4c62ee219
Matheus Melo <matheusfernando.vm@gmail.com>
2022-09-17 18:59:13 -0300

    Alterando os arquivos de banco-dados

--- banco-dados/banco_usuario.sql
+++ banco-dados/banco_usuario.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `banco` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `banco`;
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: banco
@@ -58,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-14 19:05:57
+-- Dump completed on 2022-09-17 18:56:33
