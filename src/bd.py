ad9ca1920968a0843de0a23bf109a068b06abc72
brunodenardo <brunoden.bd@gmail.com>
2022-09-29 20:41:21 -0300

    Funções que inserem requisições de depósito, requisições de mudanças de dados cadastrais e operações no banco de dados no arquivo bd.py

--- src/bd.py
+++ src/bd.py
@@ -4,6 +4,49 @@ from app import mysql, app
 import modelo
 
 
+        
+def valida(tabela, dado, valor): #valida as informações
+    cur = mysql.connection.cursor()
+    cur.execute(f"SELECT * FROM {tabela} WHERE {dado} =%s", [valor]) #Procura pelo cliente cujo CPF bata com o que foi digitado no formulário de login
+    usuario = cur.fetchone() #Armazena todas as informações desse cliente na variável usuário
+    cur.close()
+    if usuario != None:
+        return True
+    else:
+        return False
+
+
+def pegarLinha(tabela, coluna, valor): #retorna uma linha da coluna que possui o valor inserido
+    cur = mysql.connection.cursor()
+    cur.execute(f"SELECT * FROM {tabela} WHERE {coluna} =%s", [valor]) #Procura pelo cliente cujo CPF bata com o que foi digitado no formulário de login
+    linha = cur.fetchone() #Armazena todas as informações desse cliente na variável usuário
+    cur.close()
+    return linha
+
+
+def consultaSaldo(id):
+    cur = mysql.connection.cursor()
+    cur.execute(f"SELECT saldo_conta FROM conta WHERE id_usuario ={id}") #Procura pelo cliente cujo CPF bata com o que foi digitado no formulário de login
+    saldo = cur.fetchone() #Armazena todas as informações desse cliente na variável usuário
+    cur.close()
+    return saldo['saldo_conta']
+
+
+def mudaSaldo(valor, id):
+    cur = mysql.connection.cursor()
+    cur.execute(f"UPDATE conta SET saldo_conta = {valor} WHERE id_usuario = '{id}'") #Procura pelo cliente cujo CPF bata com o que foi digitado no formulário de login
+    mysql.connection.commit()
+    cur.close()
+    return None
+    
+
+
+
+
+
+#========================== Funções que inserem linhas no BD ==========================
+
+
 def criaConta(forms): #Insere uma linha com esses valores na tabela cliente
     cur = mysql.connection.cursor() #Abrindo um cursor pra navegar no SQL
     cur.execute("INSERT INTO usuario(nome_usuario, cpf_usuario, rua_avenida_usuario, numero_usuario, bairro_usuario, cidade_usuario, estado_usuario, data_nascimento_usuario, genero_usuario, senha_usuario) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (forms['nome'], forms ['cpf'], forms['rua'], forms['numero'], forms['bairro'], forms['cidade'], forms['estado'], forms['dataNascimento'], forms['genero'], forms['senha'])) # Executando o comando de inserir os dados na tabela. "%s" representa uma variável que eu defini nos parenteses seguintes
@@ -16,35 +59,29 @@ def criaConta(forms): #Insere uma linha com esses valores na tabela cliente
     cur.close() # Fechando o cursor
     return None
 
-        
-def valida(tabela, dado, valor): #valida as informações
-    cur = mysql.connection.cursor()
-    cur.execute(f"SELECT * FROM {tabela} WHERE {dado} =%s", [valor]) #Procura pelo cliente cujo CPF bata com o que foi digitado no formulário de login
-    usuario = cur.fetchone() #Armazena todas as informações desse cliente na variável usuário
-    cur.close()
-    if usuario != None:
-        return True
-    else:
-        return False
 
-def pegarLinha(tabela, coluna, valor): #retorna uma linha da coluna que possui o valor inserido
-    cur = mysql.connection.cursor()
-    cur.execute(f"SELECT * FROM {tabela} WHERE {coluna} =%s", [valor]) #Procura pelo cliente cujo CPF bata com o que foi digitado no formulário de login
-    linha = cur.fetchone() #Armazena todas as informações desse cliente na variável usuário
-    cur.close()
-    return linha
-
-def consultaSaldo(id):
-    cur = mysql.connection.cursor()
-    cur.execute(f"SELECT saldo_conta FROM conta WHERE id_usuario ={id}") #Procura pelo cliente cujo CPF bata com o que foi digitado no formulário de login
-    saldo = cur.fetchone() #Armazena todas as informações desse cliente na variável usuário
-    cur.close()
-    return saldo['saldo_conta']
-
-def mudaSaldo(valor, id):
-    cur = mysql.connection.cursor()
-    cur.execute(f"UPDATE conta SET saldo_conta = {valor} WHERE id_usuario = '{id}'") #Procura pelo cliente cujo CPF bata com o que foi digitado no formulário de login
-    mysql.connection.commit()
+#Requisição de mudança de dados cadastrais
+def reqConta_mudaDado(tabela, forms):
+    cur = mysql.connection.cursor() #Abrindo um cursor pra navegar no SQL
+    cur.execute(f"INSERT INTO {tabela}(nome_usuario, cpf_usuario, rua_avenida_usuario, numero_usuario, bairro_usuario, cidade_usuario, estado_usuario, data_nascimento_usuario, genero_usuario, senha_usuario) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (forms['nome'], forms ['cpf'], forms['rua'], forms['numero'], forms['bairro'], forms['cidade'], forms['estado'], forms['dataNascimento'], forms['genero'], forms['senha'])) # Executando o comando de inserir os dados na tabela. "%s" representa uma variável que eu defini nos parenteses seguintes
+    mysql.connection.commit() # Dando commit
     cur.close()
     return None
-    
+
+
+    #insere linha de operação realisada ou requisitada (caso depósito) na tabela desejada.
+def inserirOperacao(tabela, dic_dados): #id_usuário, operacao, valor, data e hora estão como um dicionário dicionário oq reduz esses parametros em um (será implementado na tarefa data e hora)
+    cur = mysql.connection.cursor()
+    cur.execute (f"INSERT INTO {tabela} (id_usuario, operacao, valor, data, hora) VALUES(%s, %s, %s, %s, %s)", (dic_dados['id_usuario'], dic_dados['operacao'], dic_dados['valor'], dic_dados['data'], dic_dados['hora']))
+    mysql.connection.commit() # Dando commit
+    cur.close() # Fechando o cursor
+    return None
+
+
+#Requisicao de abertura ou fechamento de conta
+def reqConta_abreFecha(tabela, id_usuário, tipo):
+    cur = mysql.connection.cursor()
+    cur.execute (f"INSERT INTO {tabela} (id_usuario, tipo) VALUES(%s, %s)", (id_usuário, tipo))
+    mysql.connection.commit() # Dando commit
+    cur.close() # Fechando o cursor
+    return None
