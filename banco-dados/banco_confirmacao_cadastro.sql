a54ced8b982c5eb1eff2e9904de4778eb4260700
Matheus Melo <matheusfernando.vm@gmail.com>
2022-10-24 08:14:39 -0300

    Adicionando a foreign key na tabela de confirmação de cadastro referente ao número da conta

--- banco-dados/banco_confirmacao_cadastro.sql
+++ banco-dados/banco_confirmacao_cadastro.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `banco` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `banco`;
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: banco
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	8.0.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -37,14 +37,15 @@ CREATE TABLE `confirmacao_cadastro` (
   `cidade_cadastro` varchar(50) NOT NULL,
   `estado_cadastro` varchar(20) NOT NULL,
   `numero_agencia` int NOT NULL,
-  `numero_conta` Int(6),
+  `numero_conta` int DEFAULT NULL,
   PRIMARY KEY (`id_cadastro`),
   KEY `fk_cadastro_agencia` (`numero_agencia`),
-  CONSTRAINT `fk_cadastro_agencia` FOREIGN KEY (`numero_agencia`) REFERENCES `gerente_agencia` (`numero_agencia`)
+  KEY `fk_cadastro_conta` (`numero_conta`),
+  CONSTRAINT `fk_cadastro_agencia` FOREIGN KEY (`numero_agencia`) REFERENCES `gerente_agencia` (`numero_agencia`),
+  CONSTRAINT `fk_cadastro_conta` FOREIGN KEY (`numero_conta`) REFERENCES `conta` (`numero_agencia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE confirmacao_cadastro ADD numero_conta Int(6);
 --
 -- Dumping data for table `confirmacao_cadastro`
 --
@@ -63,4 +64,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-10-06 19:00:56
+-- Dump completed on 2022-10-24  7:41:52
