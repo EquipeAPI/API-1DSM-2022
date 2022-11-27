71a24bc6eb5533d1a58e41e5009c6a0e1e60e533
brunodenardo <brunoden.bd@gmail.com>
2022-11-27 17:13:23 -0300

    Mudando o link do logo no login

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ app.secret_key = 'aonainfinnBFNFOANOnasfononfsa' #Chave de segurança da session
 # Configurações do banco de dados
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Goiabada2!' #Insira aqui a senha do seu servidor local do MYSQL
+app.config['MYSQL_PASSWORD'] = '' #Insira aqui a senha do seu servidor local do MYSQL
 app.config['MYSQL_DB'] = 'banco'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
@@ -549,15 +549,6 @@ def requisicoes(tipo, numero_agencia):
         requisicoes = bd.tabelaPersonalizada(str(tipo), 'numero_agencia', numero_agencia), tipo = tipo, tabelaUsuario = usuario, tabelaConta = conta, gerente = session['gerente'], tabelaAgencia =agencia, numero_agencia = numero_agencia)
 
 
-@app.route('/teste')
-def teste():
-    #dataAbertura = bd.pegarDado('usuario', 'id_usuario', 4, 'data_hora_usuario')
-    saldoAntigo = 113.78
-    rendimento = bd.pegarDado('capital_banco', 'id_capital', 1, 'taxa_rendimento')
-    valor = rendimento * saldoAntigo
-    valor = modelo.truncaValor(valor)
-    saldoFinal = saldoAntigo + (saldoAntigo * rendimento)
-    return f"Esse é um teste -> saldoAntigo:{saldoAntigo} || rendimento:{rendimento} || saldoFinal: {saldoFinal} || valor:{valor}"
 
 @app.route('/resposta/<decisao>/<tipo>/<id>')
 def respostaReq(decisao, tipo, id):
